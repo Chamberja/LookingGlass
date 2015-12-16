bdc3f9780a5e5c34798a8c1ce02c521ea98127db
Jason Ames <Chamberja_6@hotmail.com>
2015-12-16 23:36:57 +0000

    Added Broadcast Message for users connecting and disconnected

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
+// Broadcast a message to connected users when someone connects or disconnects
+// Add support for nicknames
+// Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
+// Add “{user} is typing” functionality
+// Show who’s online
+// Add private messaging
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+var connectMsg = 'Anonymous User Connected';
+var disconnectMsg = 'Anonymous User Disconnected';
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-});
-
-io.on('connection', function(socket){
+  io.emit('chat message', connectMsg);
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function(){
+    io.emit('chat message', disconnectMsg);
   });
 });
 
